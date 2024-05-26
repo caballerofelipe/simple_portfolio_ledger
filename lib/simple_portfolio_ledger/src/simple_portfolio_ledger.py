13bde8f01942b14fb3a49093338156fd9328bf21
Felipe Caballero <11650766+caballerofelipe@users.noreply.github.com>
2024-05-25 19:35:29 -0500

    In `ledger()` specifying columns to avoid the ones manually added.
    - In `SimplePortfolioLedger.ledger()` specifying columns to avoid the ones manually added.
    - TODO modifications.
    - In .vscode/settings.json, removed lines that prevented some warnings in VSCode.
    - Minor tweaks.

--- lib/simple_portfolio_ledger/src/simple_portfolio_ledger.py
+++ lib/simple_portfolio_ledger/src/simple_portfolio_ledger.py
@@ -18,14 +18,17 @@ Notes:
 _ = '''
 - For all operations
     - Add type validation
-- Deposit/withdraw
-    - If there's a cost should be stated as another operation
-    - No commission/tax, add another op for that
-    - Only an amount should be deposited or withdrew
 - Add a new column for operation id and sub id?
     - For instance, a sell is an univest and a sell, so both ops should have an id
         e.g. 132 and maybe a sub id 1 and 2
         these could go on different columns or in one column (132-1 and 132-2)
+    - The function `_add_row()` should be _add_rows (plural) to allow operation tracking and multiple rows would have the same operation id and multiple sub ids.
+- In _cols_operation* the grouping should include price_in as the computation of the same instrument with different reference instrument (price_in) wouldn't make sense (i.e. a stock bought in USD and also in CHF wouldn't allow for the computation to be consistent between the two). In the case that would be needed to be done somehow, a conversion would have to happen.
+- Deposit/withdraw
+    - If there's a cost should be stated as another operation
+    - No commission/tax, add another op for that
+    - Only an amount should be deposited or withdrew
+
 - Review _ledger_columns_attrs.
 - In _cols_operation_balance_by_instrument_for_group(), for withdraw and sell
     there should be a final review. If withdraw/sell is more than what I have
@@ -175,7 +178,6 @@ class SimplePortfolioLedger:
         pd.DataFrame
             Empty ledger DataFrame.
         """
-
         # Create an empty portfolio_ledger
         ledger = pd.DataFrame(columns=cls._ledger_columns)
 
@@ -238,7 +240,9 @@ class SimplePortfolioLedger:
         if len(self._ledger_df) == 0:
             warnings.warn('WARNING: Ledger is empty, showing only basic structure.')
 
-        the_ledger = self._ledger_df
+        # Specifying columns to avoid returning manually added columns
+        # `*` needed because self._ledger_columns is a tuple
+        the_ledger = self._ledger_df[[*self._ledger_columns]]
 
         if instrument_type is True or instrument_name is True:
             instruments = self.instruments(
@@ -714,7 +718,7 @@ class SimplePortfolioLedger:
         )
 
     # *****************
-    # MARK: Ledger Operations
+    # MARK: OPERATIONS
     # *****************
 
     def _add_row(self, value_dict):
